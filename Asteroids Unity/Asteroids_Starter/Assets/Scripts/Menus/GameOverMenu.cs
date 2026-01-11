e8f0205b8852aca428dc10377d92590e7fc5fc64
Ilse Skjelnes <ilseskje@outlook.com>
2026-01-11 23:12:15 +0100

    Final Build + Retry button changed

--- Asteroids Unity/Asteroids_Starter/Assets/Scripts/Menus/GameOverMenu.cs
+++ Asteroids Unity/Asteroids_Starter/Assets/Scripts/Menus/GameOverMenu.cs
@@ -23,7 +23,7 @@ public class GameOverMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(levelName);
         }
